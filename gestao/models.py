1693f0986de3593a09fb5f155cf82cc407e826fb
LingHun <79218633+JojoBizarreOne@users.noreply.github.com>
2026-05-01 18:36:20 +0000

    Finalização das classes Usuarios, Imoveis, FotosImoveis, Chaves, Historico

--- gestao/models.py
+++ gestao/models.py
@@ -9,7 +9,7 @@ class Usuarios(AbstractUser):
         CLIENTE = 'CLI', 'Cliente'
     CPF = models.CharField(max_length=11, primary_key=True)
     celular = models.CharField("Celular", max_length=11, unique=True)
-    contato = models.CharField("Contato",max_length=11, blank=True)
+    contato = models.CharF  ield("Contato",max_length=11, blank=True)
     email = models.EmailField("E-mail", unique=True)
     endereco = models.CharField("Endereço", max_length=100)
     tipo = models.CharField("Tipo", max_length=3, choices=Tipos.choices, default=Tipos.CLIENTE)
@@ -20,14 +20,46 @@ class Usuarios(AbstractUser):
     def __str__(self):
         return f"{self.first_name} ({self.get_tipo_display()})"
 
+class Imoveis(models.Model):
+    rua = models.CharField("Endereço", max_length=100)
+    numero = models.CharField("Número", max_length=6)
+    bairro = models.CharField("Bairro", max_length=100)
+    cidade = models.CharField("Cidade", max_length=100)
+    estado = models.CharField("Estado", max_length=100)
+    proprietario = models.ForeignKey(on_delete=models.CASCADE, Model=Usuarios.CPF)
+    observacao = models.TextField("Observações", blank=True)
+    data_criacao = models.DateField("Data de Cadastro", auto_now_add=True)
+    data_atualizacao = models.DateTimeField("Última Atualização", auto_now=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint (fields=['rua', 'numero', 'bairro', 'cidade', 'estado'], name='endereco')]
+
+    def __str__(self):
+        return f"{self.rua}, {self.numero}, {self.bairro}, {self.cidade}-{self.estado}"
+
+class FotosImoveis(models.Model):
+    imovel = models.ForeignKey(Imoveis, on_delete=models.CASCADE, related_name='fotos')
+    foto = models.ImageField("Foto", upload_to="imoveis/", blank=True, null=True)
+    descricao = models.CharField("Descrição", max_length=100, blank=True)
+
+    class Meta(models.Model):
+        verbose_name = "Foto do Imóvel"
+        verbose_name_plural = "Fotos dos Imóvel"
+
 class Chaves(models.Model):
     codigo = models.CharField("Código", primary_key=True)
     imovel = models.ForeignKey(on_delete=models.CASCADE, Model=Imoveis.endereco)
-    esta_disponivel = models.BooleanField(default=True)
-    usuario_retirada = models.ForeignKey('Usuarios', on_delete=models.CASCADE, blank=True, null=True)
-    data_retirada = models.DateTimeField()
+    esta_disponivel = models.BooleanField("Disponibilidade", default=True)
+    usuario_açao = models.ForeignKey("Usuário", on_delete=models.CASCADE)
+    data_ação= models.DateTimeField("Data", auto_now=True)
 
-class Imoveis(models.Model):
-    endereco = models.CharField("Endereço", primary_key=True)
-    proprietario = models.ForeignKey(on_delete=CASCADE, )
-# Create your models here.
+    def __str__(self):
+        return f"{self.codigo} - {self.imovel} - {self.esta_disponivel}"
+
+class Historico(models.Model):
+    usuario = models.ForeignKey(on_delete=models.SET_NULL, Model=Usuarios.CPF)
+    chave = models.ForeignKey(on_delete=models.PROTECT, Model=Chaves.codigo)
+    acao = models.ForeignKey(on_delete=models.PROTECT, Model=Chaves.esta_disponivel)        
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    
+
